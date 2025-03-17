ed48a8b1a84f159b57e2144dcf0fdbf3cdb49fd2
sourav <raavisourav@gmail.com>
2025-03-17 14:52:22 +0530

    added terrform scripst

--- task04/outputs.tf
+++ task04/outputs.tf
@@ -1,9 +1,9 @@
 output "vm_public_ip" {
   description = "The public IP address of the Virtual Machine"
-  value = azurerm_public_ip.public_ip.ip_address
+  value       = azurerm_public_ip.public_ip.ip_address
 }
 
 output "vm_fqdn" {
   description = "The fully qualified domain name of the Virtual Machine"
-  value = azurerm_public_ip.public_ip.fqdn
+  value       = azurerm_public_ip.public_ip.fqdn
 }
